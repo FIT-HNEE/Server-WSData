45e83243e907d277e80b22361624b8d7e2c53644
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-08-18 02:47:14 +0200

    adding swagger document

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, { Application, Router } from 'express';
 import morgan from "morgan";
-import Routes from './routes/index';
+import Routes from './routes';
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from '../doc/swagger.json'
 
 const PORT = process.env.PORT || 4000;
 
@@ -9,6 +11,10 @@ const app: Application = express();
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(express.static("public"));
+
+app.use(
+ '/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)
+);
 
 app.use(Routes);
 
