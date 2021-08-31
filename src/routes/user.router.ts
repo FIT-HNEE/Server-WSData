227d1d03c6196ba44c6a9883cb518237c70dfc4a
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-08-31 21:52:26 +0200

    Moving to passport js library for authentication.

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,23 +1,87 @@
 import express from 'express';
-import UserController from '../controllers/UserController';
-import { checkJwt } from '../middlewares/checkJwt';
-import { checkRole } from '../middlewares/checkRole';
+
+import passport from "../utils/passport/passport";
+
+import tokenHandler from "../middlewares/TokenHandler/TokenHandler";
+
+//import UserController from '../controllers/UserController'
+
+//import { User } from "../models/user";
+
+//import { getRepository } from 'typeorm';
+
+//import { validate } from 'class-validator';
+
+//import ApiError from '../middlewares/ErrorHandling/ApiError';
 
 const router = express.Router();
 
-//Get all users
-router.get("/",[checkJwt, checkRole(["ADMIN"])], UserController.listAll);
+router.post("/register", passport.authenticate("auth.register"), tokenHandler);
 
-// Get one user
-router.get("/:id", UserController.getOneById);
+router.post("/login", passport.authenticate("auth.login"), tokenHandler);
 
-//Create a new user
-router.post("/" ,[checkJwt, checkRole(["ADMIN"])], UserController.newUser);
+router.get("/me", passport.authenticate("auth.jwt"), tokenHandler);
 
-//Edit one user
-router.patch("/:id" ,[checkJwt, checkRole(["ADMIN"])], UserController.editUser);
+router.get("/:id", passport.authenticate("scope.me"), tokenHandler);
 
-//Delete one user
-router.delete("/:id" ,[checkJwt, checkRole(["ADMIN"])], UserController.deleteUser);
+router.get("/", passport.authenticate("scope.all"), tokenHandler)
+
+router.put("/:id", passport.authenticate("scope.edit"), tokenHandler)
+/* router.put(
+  "/:id",
+  passport.authenticate("scope.me"),
+    async (req, res, next) => {
+      
+      try {
+        //Get the ID from url
+        const id = req.params.id;
+
+        //Get values from the body
+        const { firstName, lastName, isAdmin, email, password } = req.body;
+         
+        //Try to find user on database
+          const userRepository = getRepository(User);
+          const user = await userRepository.findOneOrFail(id)
+          //Validate the new values on model
+        user.firstName = firstName;
+        user.lastName = lastName;
+        user.isAdmin = isAdmin;
+          user.email = email;
+          user.password = password;
+          user.hashPassword();
+
+        const errors = await validate(user);
+        if (errors.length > 0) {
+            res.status(400).send(errors);
+            return;
+        }
+        
+      //Try to safe, if fails, that means username already in use
+        try {
+            await userRepository.save(user);
+        } catch (error) {
+            res.status(409).send("username already in use");
+            return;
+        }
+        //After all send a 201 with updated user content
+        res.status(201).send(user)
+    } catch (e) {
+      next(e);
+    }
+  }
+); */
+
+/* router.delete(
+  "/:id",
+  passport.authenticate("scope.me"),
+  async (req, res, next) => {
+    try {
+      const user = await Users.findByIdAndDelete(req.params.id);
+      res.send(user);
+    } catch (e) {
+      next(new ApiError(500, "Delete profile is not successfull", false));
+    }
+  }
+); */
 
 export default router;
