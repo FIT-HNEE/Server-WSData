0d39a4916bc99cf3e70a9619363f8811c312861b
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-09-01 19:18:46 +0200

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,10 @@
 import express from 'express';
-import HelloController from '../controllers/hello';
-import UserRouter from "./user.router";
+
+import UserRouter from "./users";
 
 const router = express.Router();
 
-router.get("/hello", async (req, res) => {
-    const controller = new HelloController();
-    const response = await controller.getMessage();
-    return res.send(response);
-});
+
 router.use("/users", UserRouter);
 
 export default router;
