e2d2e262c1ab9a79a4503e8f20663eb70fef1de0
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-09-01 19:18:06 +0200

    Update 1629767046455-CreateAdminUser.ts

--- src/migration/1629767046455-CreateAdminUser.ts
+++ src/migration/1629767046455-CreateAdminUser.ts
@@ -1,19 +1,18 @@
 import {MigrationInterface, QueryRunner, getRepository } from "typeorm";
-import { User } from '../models/user'
+import { User } from '../models/user';
+import {Admin} from './seed'
 export class CreateAdminUser1629767046455 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
+    public async up(_: QueryRunner): Promise<any> {
+    await getRepository(User).save(
+      Admin
+        );
+        console.log('User', User)
+    
+  }
 
-        let user = new User();
-        user.email = "admin";
-        user.password = "admin";
-        user.hashPassword();
-        user.role = "ADMIN";
-        const userRepository = getRepository(User);
-        await userRepository.save(user);
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
+  public async down(_: QueryRunner): Promise<any> {
+    // do nothing
+  }
 
 }
