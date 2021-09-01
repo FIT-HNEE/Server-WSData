38dd183434a67d7c4b15c3bf757bb698c9966017
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-09-01 19:22:34 +0200

    Seed for typeorm migration file

---
+++ src/migration/seed.ts
@@ -0,0 +1,17 @@
+export const Admin = {
+  
+    email: 'admin@admin.com',
+    
+    password: 'admin',
+            
+    isAdmin: true,
+            
+    firstName: 'admin',
+            
+    lastName: 'admin',
+            
+    createdAt: `${new Date()}`,
+  
+    updatedAt: `${new Date()}`,  
+  
+};
