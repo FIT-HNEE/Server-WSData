ed868e4aabb3261e20db8a7c6e0142bbcb5aa96b
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-09-09 21:20:14 +0200

    Resend link for email verification

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -3,6 +3,16 @@ import { getRepository } from 'typeorm';
 import { validate } from 'class-validator';
 import { User } from '../models/user';
 import jwt from 'jsonwebtoken'
+import { TokenPairs } from "@utils/jwt/jwt";
+import nodemailer from 'nodemailer';
+
+const transport = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: 'pakswim@gmail.com',
+    pass: 'Moonstar@1987',
+  },
+});
 
 const {  JWT_ACCESS_SECRET } = process.env;
 
@@ -57,6 +67,41 @@ class UserController {
             
         //return res.redirect('http://localhost:3001/login');
         
+    }
+
+    static resendLink = async (req: Request, res: Response) => {
+        const { email } = req.body
+        const userRepository = getRepository(User);
+        const user = await userRepository.findOneOrFail({ where: { email } });
+         
+        if (!user || user.confirmation === true) {
+            return res
+                .status(400)
+                .json({ message: `${email} is incorrect or already verified`})            
+        } else {
+             try {        
+        const tokens = await TokenPairs({ id: user.id });        
+        const token: any = tokens.accessToken
+         const url = `http://localhost:4000/api/users/confirmation/${token}`;
+
+        await transport.sendMail({
+          from: 'pakswim@gmail.com',
+          to: email,
+          subject: "Please confirm your account",
+          html: `<h1>Email Confirmation</h1>
+              <h2>Hello ${user.lastName}</h2>
+              <p>Thank you for subscribing. Please confirm your email by clicking on the following link</p>
+              <a href="${url}"> ${url}</a>
+              </div>`,
+        })
+                 return res
+                .status(400)
+                .json({ message: `Link to ${user.lastName} has been send`, User: user })
+       } catch (e) {
+         console.log(e);
+       }
+        }
+
     }
 
 
