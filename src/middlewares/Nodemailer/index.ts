da0fc1351ad436aebd1674ee490a1da1dd5837ce
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-09-14 01:32:11 +0200

    Update for sendMail option

--- src/middlewares/Nodemailer/index.ts
+++ src/middlewares/Nodemailer/index.ts
@@ -1,9 +1,25 @@
 import nodemailer from 'nodemailer';
 
-export const transport = nodemailer.createTransport({
+const sendMail = async (email, lastName, url, message) => {
+const transport = nodemailer.createTransport({
   service: "Gmail",
   auth: {
     user: 'pakswim@gmail.com',
     pass: 'Moonstar@1987',
   },
 });
+
+  await transport.sendMail({
+    from: 'pakswim@gmail.com',    
+    to: email,                
+    subject: `${message}`,                
+    html: `<h1>${message}</h1>                
+                    <h2>Hello ${lastName}</h2>
+                    <p>Thank you for request. Please ${message} by clicking on the following link</p>
+                    <a href="${url}"> ${url}</a>
+                    </div>`,    
+  })
+}
+
+export default sendMail;
+
