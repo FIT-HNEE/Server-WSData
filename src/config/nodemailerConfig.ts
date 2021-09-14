ed33c5d9128cad5e2169404628a4047a1ca4bf52
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-09-15 01:50:50 +0200

    nodemailer middleware move to configuration folder as it makes more sense.

---
+++ src/config/nodemailerConfig.ts
@@ -0,0 +1,27 @@
+//require("dotenv").config();
+
+import nodemailer from 'nodemailer';
+
+const sendMail = async (email, lastName, url, message) => {
+const transport = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: 'pakswim@gmail.com',
+    pass: 'Moonstar@1987',
+  },
+});
+
+  await transport.sendMail({
+    from: 'pakswim@gmail.com',    
+    to: email,                
+    subject: `${message}`,                
+    html: `<h1>${message}</h1>                
+                    <h2>Hello ${lastName}</h2>
+                    <p>Thank you for request. Please ${message} by clicking on the following link</p>
+                    <a href="${url}"> ${url}</a>
+                    </div>`,    
+  }).catch(err => console.log(err));
+}
+
+export default sendMail;
+
