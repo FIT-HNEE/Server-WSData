eae18d9f96e35a1a0c76504676344b4b9e2a15e3
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-09-16 02:17:40 +0200

    Separating the port listening from app
    For overcoming the Error ; Open handle keeps Jest from exiting ( TCPSERVERWRAP)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,40 @@
+import "reflect-metadata";
+import 'module-alias/register';
+import express, { Application } from 'express';
+import morgan from "morgan";
+import Routes from './routes';
 import { createConnection } from 'typeorm';
-import dbConfig from './config/ormConfig';
+import helmet from "helmet";
+import cors from "cors";
+//import swaggerUi from "swagger-ui-express";
+//import swaggerDocument from '../doc/swagger.json'
+import dbConfig from '@config/ormConfig';
+import passport from '@utils/passport/passport';
+//import ApiError from "@middlewares/ErrorHandling/ApiError";
+//import errorHandler from '@middlewares/ErrorHandling/ErrorHandler';
+
 const PORT = process.env.PORT || 4000;
-import app from './app'
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(morgan("tiny"));
+app.use(express.static("public"));
+//app.use(errorHandler)
+/* app.use((req,res) => {
+  if(!req.route&&!res.headersSent){
+    res.status(404).send(new ApiError(404,'This route is not found',true))
+  }
+}) */
+/* app.use(
+ '/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)
+); */
+
+app.use("/api", Routes);
+app.use(cors());
+app.use(helmet());
 
 createConnection(dbConfig)
   .then((_connection) => {
@@ -12,4 +45,4 @@ createConnection(dbConfig)
   .catch((err) => {
     console.log("Not Connected to DB 👎 :", err);
     process.exit(1)
-  })
+})
