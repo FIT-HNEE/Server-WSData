8b0144837cee6285e2d8087661c601bf5c6f472a
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-09-23 21:29:42 +0200

    Update ApiError.ts

--- src/middlewares/ErrorHandling/ApiError.ts
+++ src/middlewares/ErrorHandling/ApiError.ts
@@ -1,11 +1,13 @@
+import chalk from 'chalk';
 class ApiError {
-    code: number
-    isOperational: boolean
-    message: string
+    readonly code: number
+    readonly isOperational: boolean
+    readonly message: string
     constructor(code: number = 500, message: string = 'An error occured', isOperational: boolean = true) {
         this.code = code
         this.isOperational = isOperational || this.code === 500
         this.message = message
+        console.log(chalk.red.bold(`❌ (${code}) - ${this.message}`));
     }
 };
 
