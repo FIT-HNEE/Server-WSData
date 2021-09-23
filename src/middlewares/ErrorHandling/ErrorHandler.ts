04a7a30fdc6c13959d2e4b3ea152cb17845302fe
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-09-23 21:29:47 +0200

    Update ErrorHandler.ts

--- src/middlewares/ErrorHandling/ErrorHandler.ts
+++ src/middlewares/ErrorHandling/ErrorHandler.ts
@@ -1,6 +1,20 @@
-const errorHandler =(error,_req,res,_next) => {
-  if(error&&!res.headersSent){
-    res.status(error.code).send(error)
+import { NextFunction, Request, Response } from 'express';
+import ApiError from './ApiError';
+
+const errorHandler = (
+  error: Error | ApiError,
+  _req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (error && !res.headersSent) {
+    const errorResponse =
+      error instanceof ApiError
+        ? error
+        : new ApiError(500, error.message, true);
+    res.status(errorResponse.code).send(errorResponse)
+  } else {
+    next()
   }
 }
 
