16433c9df8420fc463010ba01ce109ab0142613a
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-09-26 07:04:02 +0200

    Documentation of the API by using Swagger UI

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,8 @@ import morgan from "morgan";
 import Routes from './routes';
 import helmet from "helmet";
 import cors from "cors";
-//import swaggerUi from "swagger-ui-express";
-//import swaggerDocument from '../doc/swagger.json'
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from './doc/swagger.json'
 import passport from './utils/passport/passport';
 //import ApiError from "@middlewares/ErrorHandling/ApiError";
 //import errorHandler from '@middlewares/ErrorHandling/ErrorHandler';
@@ -20,14 +20,15 @@ app.use(passport.session())
 app.use(morgan("tiny"));
 app.use(express.static("public"));
 //app.use(errorHandler)
-/* app.use((req,res) => {
-  if(!req.route&&!res.headersSent){
-    res.status(404).send(new ApiError(404,'This route is not found',true))
+
+/* app.use((req: Request, res: Response) => {
+  if (!req.route && !res.headersSent) {
+    res.status(404).send(new ApiError(404, 'This route is not found', true));
   }
-}) */
-/* app.use(
+}); */
+app.use(
  '/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)
-); */
+);
 
 app.use("/api", Routes);
 app.use(cors());
