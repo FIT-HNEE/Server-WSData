3da77c935e3135fb3b70cfaac239f0603b62f81f
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-09-27 22:56:10 +0200

    moving cors up and removing other options

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,8 @@ import passport from './utils/passport/passport';
 
 const app: Application = express();
 
+app.use(cors())
+
 app.use(express.json());
 app.use(passport.initialize())
 app.use(passport.session())
@@ -29,34 +31,8 @@ app.use(express.static("public"));
 app.use(
  '/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)
 );
-/* app.use(function (_req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    //res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-}); */
-/* const corsOptions = {  
-    origin:"http://localhost:3000", 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-
-app.use(cors(corsOptions)); */
-app.use(cors())
 app.use("/api", Routes);
-
 app.use(helmet());
 
 
