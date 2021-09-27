2432caaf65c698cf2df66173befe432465c1d9a2
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-09-27 22:54:43 +0200

    Different options for cors using middleware, app.use with function and cors option
    Note: Always use the cors above the api routes.

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import express, { Application } from 'express';
 import morgan from "morgan";
 import Routes from './routes';
 import helmet from "helmet";
-import cors from "cors";
+import cors from "./middlewares/cors/cors";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from './doc/swagger.json'
 import passport from './utils/passport/passport';
@@ -29,9 +29,34 @@ app.use(express.static("public"));
 app.use(
  '/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)
 );
+/* app.use(function (_req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    //res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+}); */
+/* const corsOptions = {  
+    origin:"http://localhost:3000", 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+
+app.use(cors(corsOptions)); */
+app.use(cors())
 app.use("/api", Routes);
-app.use(cors());
+
 app.use(helmet());
 
 
