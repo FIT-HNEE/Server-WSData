3dd8573cfda8cfe596ab973608bec7d624b9e597
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-10-11 23:06:07 +0200

    Addition of weather data route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,13 @@ import express from 'express';
 
 import UserRouter from "./users";
 
+import WeatherDataRoute from './weatherData'
+
 const router = express.Router();
 
 
 router.use("/users", UserRouter);
+
+router.use("/weatherData", WeatherDataRoute);
 
 export default router;
