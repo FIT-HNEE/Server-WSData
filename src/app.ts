0ac44a5fff846bdd3c417a65fc36f5397f7270c3
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-10-11 23:55:35 +0200

    running the schedule for every 3 days in month
    The / defines steps, that means */12 will execute every 12 days, starting from 1, that means: 1st, 13th (for all months) and also 25th .

--- src/app.ts
+++ src/app.ts
@@ -15,8 +15,8 @@ import cron from 'node-cron';
 import UserController from "./controllers/UserController";
 
 
-cron.schedule('* * * * *', UserController.sendReminder);
-cron.schedule('* * * * *', UserController.deleteDeactivatedAccount);
+cron.schedule('0 0 */12 * *', UserController.sendReminder);   
+cron.schedule('0 0 */12 * *', UserController.deleteDeactivatedAccount);
 
 const app: Application = express();
 
