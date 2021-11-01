569ead3489dbc4b714b20637dec764c373b30208
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-11-01 18:13:00 +0100

    Last Seven Days Data Fetching

--- src/controllers/WeatherDataController.ts
+++ src/controllers/WeatherDataController.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from 'express';
 import fetch from 'node-fetch';
 const { API_KEY_WEATHER_DATA, WEBVIS_DATA_PROVIDER } = process.env
-
+import moment from 'moment'
 class WeatherDataController {
 
-    static weatherData = async (req: Request, res: Response) => {
+    static selectiveWeatherData = async (req: Request, res: Response) => {
         const { StationName, StartDay, EndDay} = req.body
         try {
             let url = `${WEBVIS_DATA_PROVIDER}/api/${API_KEY_WEATHER_DATA}/${StationName}/${StartDay}/${EndDay}`
@@ -21,7 +21,32 @@ class WeatherDataController {
         } catch (error) {
             console.log(error);            
         }    
-    }   
+    }
+    
+    static lastSevenDaysWeatherData = async (_req: Request, res: Response) => {
+        
+        const StationName = 'Buche'
+        const currentDate = new Date()
+        const EndDate = moment(currentDate).format('YYYY-MM-DD')
+        const LastSevenDays = moment(currentDate).subtract(6, 'days').toDate();
+        const StartDate = moment(LastSevenDays).format('YYYY-MM-DD')
+        
+        try {
+            let url = `${WEBVIS_DATA_PROVIDER}/api/${API_KEY_WEATHER_DATA}/${StationName}/${StartDate}/${EndDate}`
+            
+            await fetch(url)
+                .then(resp => resp.json())
+                .then(data => {
+                    res.send(data)
+                })
+                .catch(err => {                
+                    console.log(err)                    
+                })
+            
+        } catch (error) {
+            console.log(error);            
+        }    
+    } 
 }
 
 export default WeatherDataController; 
