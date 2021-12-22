41a2acfe97fb4fe9a77bc4e948048dfdb24a1051
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-12-23 00:34:43 +0100

    weather forecast route

--- src/routes/weatherData/index.ts
+++ src/routes/weatherData/index.ts
@@ -7,6 +7,7 @@ const router = express.Router();
 //Routes for Weather Data
 
 router.get("/lastSevenDays", WeatherDataController.lastSevenDaysWeatherData);
+router.get("/forecast", WeatherDataController.weatherDataForecast);
 router.post("/", WeatherDataController.selectiveWeatherData);
 
 export default router;
